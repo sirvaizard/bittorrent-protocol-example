ce0c94f60c9e8dae291f648208510c61c8cbcdf4
Victor Lopes <thevictorfl@gmail.com>
2021-11-08 02:15:10 -0300

    add announce function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dgram from 'dgram'
 import { Buffer } from 'buffer'
 import { URL } from 'url'
 import bencode from 'bencode'
+import bignum from 'bignum'
 
 const CONNECT = 0
 const ANNOUNCE = 1
@@ -16,6 +17,14 @@ const torrentFile = await fs.readFile(torrentFilename)
 const torrent = bencode.decode(torrentFile, 'utf8')
 const torrentInfo = bencode.encode(torrent.info)
 const infoHash = crypto.createHash('sha1').update(torrentInfo).digest()
+
+const leftDownload = torrent.info.length ? torrent.info.length :
+    torrent.info.files.reduce((acc, curr) => acc + curr.length, 0)
+
+const leftDownloadBuffer = bignum.toBuffer(leftDownload, {size: 8})
+
+const peerId = crypto.randomBytes(20)
+Buffer.from('-VICTOR-').copy(peerId, 0)
 
 // for (const [key, value] of Object.entries(torrent)) {
 //     console.log(`[${key}]: ${value}`)
@@ -66,6 +75,11 @@ UDPSocket.on('message', (msg, rinfo) => {
 
             console.log(`Connect`)
             scrape(connectionIdHigh, connectionIdLow, transactionId)
+            announce(connectionIdHigh, connectionIdLow)
+            break
+
+        case ANNOUNCE:
+            console.log('Announce')
             break
 
         case SCRAPE:
@@ -94,8 +108,8 @@ UDPSocket.on('message', (msg, rinfo) => {
 
 // O announce padrão não estava me retornando resposta, então peguei
 // o primeiro da announce-list que me retornou algo.
-const announce = torrent['announce-list'][2][0]
-const url = new URL(announce)
+const announceURL = torrent['announce-list'][2][0]
+const url = new URL(announceURL)
 UDPSocket.send(connectionRequest, 0, connectionRequest.length, url.port, url.hostname, (error, _) => {
     if (error) {
         console.log(`error: ${error}`)
@@ -118,6 +132,50 @@ function scrape (connectionIdHigh, connectionIdLow, transactionId) {
     scrapeRequest.write(infoHash.toString(), 16, scrapeRequest.length, 'hex')
 
     UDPSocket.send(scrapeRequest, 0, scrapeRequest.length, url.port, url.hostname, (error, _) => {
+        if (error)
+            console.log(`error: ${error}`)
+    })
+}
+
+function announce (connectionIdHigh, connectionIdLow) {
+    // Offset  Size    Name    Value
+    // 0       64-bit integer  connection_id
+    // 8       32-bit integer  action          1 // announce
+    // 12      32-bit integer  transaction_id
+    // 16      20-byte string  info_hash
+    // 36      20-byte string  peer_id
+    // 56      64-bit integer  downloaded
+    // 64      64-bit integer  left
+    // 72      64-bit integer  uploaded
+    // 80      32-bit integer  event           0 // 0: none; 1: completed; 2: started; 3: stopped
+    // 84      32-bit integer  IP address      0 // default
+    // 88      32-bit integer  key
+    // 92      32-bit integer  num_want        -1 // default
+    // 96      16-bit integer  port
+    // 98
+    const transactionId = Math.floor((Math.random()*100000)+1);
+    const announceRequest = Buffer.alloc(98, 0)
+    // connection_id
+    announceRequest.writeUInt32BE(connectionIdHigh, 0)
+    announceRequest.writeUInt32BE(connectionIdLow, 4)
+    // action
+    announceRequest.writeUInt32BE(ANNOUNCE, 8)
+    // transaction_id
+    announceRequest.writeUInt32BE(transactionId, 12)
+    // info_hash
+    infoHash.copy(announceRequest, 16)
+    // peer_id
+    peerId.copy(announceRequest, 36)
+    // left
+    leftDownloadBuffer.copy(announceRequest, 64)
+    // key
+    crypto.randomBytes(4).copy(announceRequest, 88)
+    // num_want
+    announceRequest.writeInt32BE(-1, 92)
+    // port
+    announceRequest.writeUInt16BE(url.port, 96)
+
+    UDPSocket.send(announceRequest, 0, announceRequest.length, url.port, url.hostname, (error, _) => {
         if (error)
             console.log(`error: ${error}`)
     })
